f055596f29222453afafb8278d7dc0b6e9b73168
Stephanie Sara Chong <stephaniesarachong@gmail.com>
2018-01-09 16:06:00 +0800

    Implements stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,47 @@
 
 // but you don't so you're going to write it from scratch:
 
+function type(obj, type) {
+  return (typeof obj) === type;
+}
+
+// console.log(type(undefined, 'undefined')); // true
+// console.log(type(undefined, 'number')); // false
+// console.log(type('hello', 'string')); // true
+// console.log(type('hello', 'boolean')); // fase
+// console.log(type(true, 'boolean')); // true
+// console.log(type(true, 'string')); // false
+
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (type(obj, 'string')) {
+    return '"' + obj + '"';
+  } else if (type(obj, 'number') || type(obj, 'boolean')) {
+    return '' + obj;
+  } else if (type(obj, 'undefined') || type(obj, 'function')) {
+    return null;
+  } else if (obj == null) {
+    return 'null';
+
+  // If obj is an array
+  } else if (Array.isArray(obj)) {
+    return '[' + obj.map(function(elem) {
+        return stringifyJSON(elem);
+      }).join(',') + ']';
+
+  // If obj is a non-array object (with key-value pairs)
+  } else {
+    var pairs = [];
+    Object.keys(obj).forEach(function(key) {
+      var value = stringifyJSON(obj[key]);
+      if (value != null) {
+        pairs.push(stringifyJSON(key) + ':' + value);
+      }
+    });
+    return '{' + pairs.join(',') + '}';
+
+    // return '{' + Object.keys(obj).map(function(key) {
+    //   return stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+    // }).join(',') + '}';
+  }
 };
